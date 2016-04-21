5c2008e1333e08f9653a890b65cba813150bb585
Jessica Cruz <jessicamc514@gmail.com>
2016-04-20 23:13:49 -0400

    April 20th 2016 Classwork

---
+++ assignment_3_temperature.py
@@ -0,0 +1,16 @@
+def main():
+    tempprogram()
+
+def tempprogram():
+    a=True
+    while(a==True):
+        fahrenheittemp = int(input("What is the current temperature in Fahrenheit? "))
+        celsiustemp = ((fahrenheittemp - 32) * (5 / 9))
+        print("The temperature is ", fahrenheittemp, " in Fahrenheit and ", celsiustemp, " in Celsius.")
+        answer = input("Would you like to convert another temperature? ")
+        if answer == 'Yes' or answer=='yes':
+            a=True
+        else:
+            a= False
+
+main()
