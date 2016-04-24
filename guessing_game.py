b572bef220e3b19c1d4131b0c55a57d736329438
Jessica Cruz <jessicamc514@gmail.com>
2016-04-23 23:21:56 -0400

    Assignment # 3.4- Guessing Game

--- guessing_game.py
+++ guessing_game.py
@@ -4,10 +4,19 @@ def main():
     print("Welcome to the number guessing game. Please guess a number between 1-10.")
     print(x)
     guess = int(input("What is your number guess? "))
-    while x!=guess:
-        if x!=guess:
+    counter=1
+    while counter!=3 and x!=guess:
+        if counter!=3 and x!=guess:
+            if guess==(x-2) or guess==(x+2):
+                print("Warm")
+            elif guess==(x-1) or guess==(x+1):
+                print("Hot")
+            else:
+                print("Cold")
             print("Incorrect guess. Please try again.")
             guess = int(input("What is your number guess? "))
+            counter=counter+1
             if x == guess:
-                print("Congratulation! You won. Thanks for play.")
+                print("Congratulation! You won. Thanks for playing.")
+    print("Too many guesses. Please play again.")
 main()
