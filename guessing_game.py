73929510759baba5878bda90879b486206895c48
Jessica Cruz <jessicamc514@gmail.com>
2016-04-26 08:45:05 -0400

    Assignment # 3.4- Guessing Game

--- guessing_game.py
+++ guessing_game.py
@@ -2,7 +2,7 @@ import random
 x=random.randint(1,10)
 def main():
     print("Welcome to the number guessing game. Please guess a number between 1-10.")
-    print(x)
+    #print(x)
     guess = int(input("What is your number guess? "))
     counter=1
     while counter!=3 and x!=guess:
