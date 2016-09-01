12c920a9db72b3cb49d5cf8a9fd32477e306b919
Jessie <jessicamc514@gmail.com>
2016-08-31 20:26:30 -0400

    Warm hot chilly

--- assignment_3_temperature.py
+++ assignment_3_temperature.py
@@ -2,6 +2,7 @@ def main():
     tempprogram()
 
 def tempprogram():
+    x="cookies"
     a=True
     while(a==True):
         fahrenheittemp = int(input("What is the current temperature in Fahrenheit? "))
