1712026c0911d5e556eeb403768f0b67ebddea5f
Jessie <jessicamc514@gmail.com>
2016-09-06 09:25:13 -0400

    Fun with loops

--- Loops.py
+++ Loops.py
@@ -4,7 +4,7 @@
     elif (numlist % 3  == 0):
         print(numlist,"Divisible by 3")
     else:
-        print (numlist,"Prime number")'''
+        print (numlist,"Prime number")
 
 a=2
 while a<=100:
@@ -21,3 +21,17 @@ while a<=100:
     else:
         print(a, "Prime number")
         a = a + 1
+
+for b in range (2,101):
+    if (b / 2 > 1 and b % 2 == 0) or (b / 3 > 1 and b % 3 == 0) or (b / 5 > 1 and b % 5 == 0) or (b / 7 > 1 and b % 7 == 0) or (b / 9 > 1 and b % 9 == 0):
+        print(end="")
+    else:
+        print(b, "Prime number")
+
+a=2
+while a<=100:
+    if (a/2 >1 and a % 2 == 0) or (a/3 >1 and a % 3 == 0) or (a/5 >1 and a % 5 == 0) or (a/7 >1 and a % 7 == 0) or (a/9 >1 and a % 9 == 0):
+        a = a + 1
+    else:
+        print(a, "Prime number")
+        a = a + 1'''
